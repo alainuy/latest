c862e371069a6641d95778f9a2c7eea954e01858
alainuy <blitzkin@gmail.com>
2018-03-28 01:39:41 +0800

    relationship might work

--- app/Http/Controllers/AttendancesController.php
+++ app/Http/Controllers/AttendancesController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use App\User;
+use App\Attendance;
+use DB;
 
 class AttendancesController extends Controller
 {
@@ -41,8 +43,26 @@ class AttendancesController extends Controller
         $emp_id = $request->emp_id;
 
         // verify first if emp_id entered is found in user db
-        if (User::where('emp_id', '=', Input::get('emp_id'))->exists()) {
-            return 'emp_id to be save in db';
+ 
+        // if (User::where('emp_id', '=', Input::get('emp_id'))->exists()) {
+        if (User::where('emp_id', '=', $emp_id)->exists()) {
+
+
+            $usr_id = DB::table('users')->where('emp_id', '=', $emp_id)->select('id')->get();
+            
+            
+            
+            // $post = new Attendance;
+            // $post->emp_id = $request -> input('title');
+            // $post->body = $request -> input('body');
+            // $post->user_id = auth()->user()->id;
+            // $post->save();
+    
+            
+            // return 'emp_id to be save in db';
+            return $usr_id;
+
+
         }      
         else {
             return 'Employee ID NOT FOUND';
